2d75eeb266e9ca54ed3c240329283b63efd8d201
Akubrecah <poweldayck@gmail.com>
2025-08-15 00:50:34 +0300

    final commit

--- control_flows_&_functions.py
+++ control_flows_&_functions.py
@@ -1,2 +1,15 @@
-def dicount_calculatro(price, percentage_discount):
-    
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        return price * (discount_percent/100)
+    return price
+
+marked_price = float(input("Enter the Marked Price: "))
+discount = float(input("Enter the discount percentage: "))
+
+buying_price = calculate_discount (marked_price, discount)
+
+if discount >= 20:
+    print(f"Your Total Cost is: {buying_price}")
+
+else:
+    print(f"No discount applied. Price remains: {buying_price}")
